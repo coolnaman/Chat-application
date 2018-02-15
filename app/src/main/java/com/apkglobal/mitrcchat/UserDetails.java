c99f7623f116f454f814324abcdc098e0b349a0e
coolnaman <coolnaman112@gmail.com>
2018-02-15 15:04:30 +0530

    chat app

---
+++ app/src/main/java/com/apkglobal/mitrcchat/UserDetails.java
@@ -0,0 +1,16 @@
+package com.apkglobal.mitrcchat;
+
+/**
+ * Created by Lenovo on 13-02-2018.
+ */
+
+public class UserDetails {
+    /*user  data from the server
+    *
+    * */
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
+
+
